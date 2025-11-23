9f1df52dc3ff84bfc6dc8bc589294970a8f0c084
Matt <probably0k@proton.me>
2025-11-24 02:00:58 +1000

    Implemented all linked list push tests (made old ones rigorous and one for at specific index). These double as get tests as well.

--- tests/test_linked_list.c
+++ tests/test_linked_list.c
@@ -17,33 +17,57 @@ void teardown_linked_list(void) { linked_list_free(list); }
 
 /**
  * Test pushing an element to the end of the of the linked list a certain
- * number of times.
+ * number of times. Aditionally tests getting the last element on each push.
  */
-void test_linked_list_push_last(void) {
+void test_linked_list_push_get_last(void) {
     bool pushError;
-    size_t max = 1024;
-    for (size_t i = 0; i < max; i++) {
+    uint32_t max = 1024;
+    for (uint32_t i = 0; i < max; i++) {
         pushError = linked_list_push_last(list, (void*)&i);
 
         TEST_ASSERT_FALSE(pushError);
-        TEST_ASSERT_EQUAL_size_t(i + 1, list->size);
+        TEST_ASSERT_EQUAL_size_t((size_t)(i + 1), list->size);
         TEST_ASSERT_EQUAL_UINT32(i, *(uint32_t*)linked_list_get_last(list));
     }
 }
 
 /**
  * Test pushing an element to the start of the of the linked list a certain
- * number of times.
+ * number of times. Aditionally tests getting the first element on each push.
  */
-void test_linked_list_push_first(void) {
+void test_linked_list_push_get_first(void) {
     bool pushError;
-    size_t max = 1024;
-    for (size_t i = 0; i < max; i++) {
+    uint32_t max = 1024;
+    for (uint32_t i = 0; i < max; i++) {
         pushError = linked_list_push_first(list, (void*)&i);
 
         TEST_ASSERT_FALSE(pushError);
-        TEST_ASSERT_EQUAL_size_t(i + 1, list->size);
+        TEST_ASSERT_EQUAL_size_t((size_t)(i + 1), list->size);
         TEST_ASSERT_EQUAL_UINT32(i, *(uint32_t*)linked_list_get_first(list));
+    }
+}
+
+/**
+ * Test pushing an element at specific index of a list already containing two
+ * elements (in the middle). Aditionally tests getting the second element on
+ * each push.
+ */
+void test_linked_list_push_get_index(void) {
+    // Insert between two existing elements
+    uint32_t add = 0;
+    linked_list_push_last(list, (void*)&add);
+    linked_list_push_last(list, (void*)&add);
+
+    bool pushError;
+    uint32_t max = 1024;
+
+    for (uint32_t i = 1; i <= max; i++) {
+        // Push after first index
+        pushError = linked_list_push_index(list, (void*)&i, 1);
+
+        TEST_ASSERT_FALSE(pushError);
+        TEST_ASSERT_EQUAL_size_t((size_t)(i + 2), list->size);
+        TEST_ASSERT_EQUAL_UINT32(i, *(uint32_t*)linked_list_get_index(list, 1));
     }
 }
 
@@ -53,8 +77,9 @@ void run_linked_list_tests(void) {
     teardownHandler = teardown_linked_list;
 
     // Run tests
-    RUN_TEST(test_linked_list_push_last);
-    RUN_TEST(test_linked_list_push_first);
+    RUN_TEST(test_linked_list_push_get_last);
+    RUN_TEST(test_linked_list_push_get_first);
+    RUN_TEST(test_linked_list_push_get_index);
 
     // Cleanup handlers
     setupHandler = NULL;
